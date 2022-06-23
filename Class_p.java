9f1dd6d5c696715021122d374a0fdce85e86d8e2
sureshmaraboina <108087913+sureshmaraboina@users.noreply.github.com>
2022-06-23 22:54:57 +0530

    Add files via upload
    Class_p: Add: Basic c function

---
+++ Class_p.java
@@ -0,0 +1,31 @@
+import java.lang.reflect.*;
+public class Class_p
+{
+ public static void main(String[] args)
+	{
+	Add nene = new Add();
+     Add nuvvu = new Add();
+           
+		System.out.println("hai");
+		System.out.println(nene.a);
+		//System.out.println(nuvvu.a);
+            nene.Naresh();
+	}
+}
+class Add
+{
+    private int a;
+	int b=20;
+ String str = "hyderabad";
+ public static void Naresh()
+{
+System.out.println("Add Class Printing...");
+}
+public void seta(int a) {
+    this.a = a;
+  }
+public int geta() {
+    return this.a;
+  }
+  
+}
